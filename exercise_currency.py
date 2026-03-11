aeff9f302cabbcbba644753db06ff0ecbc26b835
oliviapimentel1 <opimentel@mail.austral.edu.ar>
2026-03-11 18:36:39 -0300

    Fix currency conversion calculations

--- exercise_currency.py
+++ exercise_currency.py
@@ -11,9 +11,9 @@ def currency():
     tasa_dolar = 1500  # 1 dólar = 1500 pesos
     tasa_euro = 1600   # 1 euro = 1600 pesos
     tasa_real = 250    # 1 real = 250 pesos
-    monto_dolares = (tasa_dolar * pesos)
+    monto_dolares = (pesos / tasa_dolar)
     print (monto_dolares)
-    monto_euros = (tasa_euro * pesos)
+    monto_euros = (pesos / tasa_euro)
     print(monto_euros)
-    monto_reales = (tasa_real * pesos)
+    monto_reales = (pesos / tasa_real)
     print(monto_reales)
