5dbf3c4ec90b12319f2ad55a2a546cdfbd332d6d
oliviapimentel1 <opimentel@mail.austral.edu.ar>
2026-03-11 18:23:52 -0300

    Add basic arithmetic operations in exercise_math.py

--- exercise_math.py
+++ exercise_math.py
@@ -13,3 +13,17 @@ def math():
     """
     a = 57
     b = 7
+    la_suma = (a + b)
+    print (la_suma)
+    la_diferencia = (a - b)
+    print (la_diferencia)
+    el_producto = (a*b)
+    print (el_producto)
+    el_promedio = ( (a+ b) / 2)
+    print (el_promedio)
+    el_cociente_entero = (a // b)
+    print (el_cociente_entero)
+    el_resto = (a%b)
+    print (el_resto)
+    valor_real = (a / b)
+    print (valor_real)
